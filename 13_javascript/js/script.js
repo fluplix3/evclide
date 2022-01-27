c6a064c4f2e0482d99da2258ec800e86079b7ac5
artyom grachev <artemgrachev933@mail.ru>
2022-01-27 19:47:42 +0300

    burger-script added

--- 13_javascript/js/script.js
+++ 13_javascript/js/script.js
@@ -1,3 +1,5 @@
+// Slider
+
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   spaceBetween: 70,
@@ -12,18 +14,43 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
+// Tabs
+
 document.querySelectorAll('.work__tabs-btn').forEach(function (tabsBtn) {
   tabsBtn.addEventListener('click', function (e) {
     const path = e.currentTarget.dataset.path;
     document.querySelectorAll('.work__tabs-btn').forEach(function (btn) {
       btn.classList.remove('work_one-tab')
     });
-    e.currentTarget.classList.add('tabs-nav__btn--active');
+    e.currentTarget.classList.add('work_one-tab');
     document.querySelectorAll('.work__tabs-item').forEach(function (tabsBtn) {
       tabsBtn.classList.remove('work__tabs-item-active')
     });
     document.querySelector(`[data-target="${path}"]`).classList.add('work__tabs-item-active');
-    });
+  });
 });
+
+// Accordion
+
+$(function () {
+  $("#accordion").accordion({
+    active: false,
+    collapsible: true
+  });
+});
+
+// Burger
+
+window.addEventListener('DomContentLoaded', function () {
+  document.querySelector('#burger').addEventListener('click', function () {
+    document.querySelector('#menu').classList.toggle('is-active')
+  })
+
+  document.querySelectorAll('text').forEach(function(el) {
+    el.addEventListener('click', function(event) {
+      event.target.classList.toggle('alert')
+    })
+  })
+})
 
 
